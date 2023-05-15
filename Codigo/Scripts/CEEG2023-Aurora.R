6a74e9250e31268c900a07073e182e92483b7c87
Aur0ra10 <129422314+Aur0ra10@users.noreply.github.com>
2023-05-15 15:17:10 -0600

    8 indicadores cap 2
    Se completo 8 indicadores del cap 2, estan pendientes 2 indicadores sobre anticoncepción.

--- Codigo/Scripts/CEEG2023-Aurora.R
+++ Codigo/Scripts/CEEG2023-Aurora.R
@@ -210,9 +210,17 @@ PEATOTAL_18 <- sum(PEA_18$FACTOR)
 ############################################################################
 
 ################################################################################
-# 2.1.	Número de casos de mujeres embarazadas entre 15 y 49 años por Pueblo   #
-# (serie histórica de 2018 a 2022)                                             #
+# 2.1.	Número de casos de mujeres embarazadas entre 10 y 55 años atendidas por
+# el sistema de salud pública (serie histórica de 2018 a 2022)                                             #
 ################################################################################
+Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
+
+c2_01 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_1")) %>%
+  rename("2018" = X2018, "2019" = X2019, "2020" = X2020, "2021" = X2021, 
+         "Grupos de edad" = Edad)
+
+# Enviar a Latex
+Tabla2_01 <- tablaLaTeX(c2_01, ruta = paste0(directorioGraficas, "Tabla2_01.tex"))
 
 ################################################################################
 # 2.2.	Población entre 15 y 49 años que han usado algún método de             #
@@ -250,67 +258,75 @@ c2_05 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_5"))%>%
 Tabla2_05 <- tablaLaTeX(c2_05, ruta = paste0(directorioGraficas, "Tabla2_05.tex"))
 
 ################################################################################
-# 2.6.	Discapacidad por sexo, según grupos de edad                            #
-################################################################################
-
-################################################################################
-# 2.7.	Discapacidad por sexo, según Pueblos                                   #
-################################################################################
-
-################################################################################
-# 2.8.	Porcentaje de personas notificadas con VIH/SIDA, por sexo, según       #
+# 2.6.	Porcentaje de personas notificadas con VIH/SIDA, por sexo, según       #
 # grupos de edad (serie histórica de 2018 a 2023)                              #
 ################################################################################
 
 Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
 
-c2_08 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_8"))
+c2_06 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_6"))
 
+c2_06$z <- factor(c2_06$z, levels = c("Mujeres", "Hombres"))
 
+#Grafica g2_08
+g2_06 <- graficaApilada(c2_06, categoria_leyenda = "", tipo = "columna", 
+                                     decimales = TRUE, leyenda = "arriba")
 
-
-
+# Enviar a Latex
+exportarLatex(nombre = paste0(directorioGraficas, "g2_06.tex"), graph = g2_06)
 
 ################################################################################
-# 2.9.	Número de casos de mujeres seropositivas embarazadas entre 15 y 49 años# 
+# 2.7.	Número de casos de mujeres seropositivas embarazadas entre 15 y 49 años# 
 # por Pueblo (serie histórica de 2018 a 2022)                                  #
-################################################################################
-
-################################################################################
-# 2.10.	Tasa de mortalidad materna, según dominio de estudio                   #
-#(serie histórica de 2018 a 2022)                                              #
 ################################################################################
 Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
 
-c2_10 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_10"))
+c2_07 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_7")) %>%
+  rename("2018" = X2018, "2019" = X2019, "2020" = X2020, "2021" = X2021, "2022" = X2022)
 
-g2_10 <- graficaLinea(c2_10, inicio = -1, ancho = 1.5, precision=2, escala = "normal", rotar = F, final = 1)
+# Enviar a Latex
+Tabla2_07 <- tablaLaTeX(c2_07, ruta = paste0(directorioGraficas, "Tabla2_07.tex"))
+
+
+################################################################################
+# 2.8.	Tasa de mortalidad materna (serie histórica de 2018 a 2022)                                              #
+################################################################################
+Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
+
+c2_08 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_8"))
+
+g2_08 <- graficaLinea(c2_08, inicio = -1, ancho = 1.5, precision=2, escala = "normal", rotar = F, final = 1)
 # g3_03 <- etiquetasLineas(graph = g3_03, posiciones = c(0,1,2,3,4))
 
 
 # Enviar a Latex
-exportarLatex(nombre = paste0(directorioGraficas,"g2_10.tex"),
-              graph = g2_10, preambulo = F)
-################################################################################
-# 2.11.	Tasa de mortalidad materna por causa de muerte                         #
-################################################################################
-
+exportarLatex(nombre = paste0(directorioGraficas,"g2_08.tex"),
+              graph = g2_08, preambulo = F)
 
 ################################################################################
-# 2.12.	Defunciones por sexo y Pueblo, según 10 principales causas de muerte   #
+# 2.9.	Número de casos mortalidad materna según causa de muerte                     #
+################################################################################
+Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
+
+c2_09 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_9")) %>%
+  rename("2018" = X2018, "2019" = X2019, "2020" = X2020, "2021" = X2021) %>%
+  rename("Causa de Muerte" = Causas.de.Muerte)
+
+# Enviar a Latex 
+Tabla2_09 <- tablaLaTeX(c2_09, ruta = paste0(directorioGraficas, "Tabla2_09.tex"))
+
+################################################################################
+# 2.10.	Defunciones por sexo y Pueblo, según 10 principales causas de muerte   #
 # (comparar 2018 y 2022)                                                       #
 ################################################################################
 
 Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
-
-c2_12 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_12")) %>%
+c2_10 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_10")) %>%
   rename("Causa de Muerte" = Causa.de.muerte)
 
+
 # Enviar a Latex 
-Tabla2_12 <- tablaLaTeX(c2_12,nombre_grupos = c(" ", "Pueblo de pertenencia" = 6), 
-                        opacidad_filas = 0.5, ruta = paste0(directorioGraficas, "Tabla2_12.tex"))
-
-
-
+Tabla2_10 <- tablaLaTeX(c2_10,nombre_grupos = c(" ", "Pueblo de pertenencia" = 6), 
+                        opacidad_filas = 0.5, ruta = paste0(directorioGraficas, "Tabla2_10.tex"))
 
 

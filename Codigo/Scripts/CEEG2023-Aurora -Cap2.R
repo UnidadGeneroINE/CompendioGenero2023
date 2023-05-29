99a6fd13381f5a35ac1753b177e27d9c365851f4
Aur0ra10 <129422314+Aur0ra10@users.noreply.github.com>
2023-05-29 10:38:08 -0600

    Indicadores de acticoncepción
    Se agregaron 2 indicadores sobre métodos de planificación familiar

--- Codigo/Scripts/CEEG2023-Aurora -Cap2.R
+++ Codigo/Scripts/CEEG2023-Aurora -Cap2.R
@@ -36,6 +36,7 @@ directorioBases <- "C:\\Users\\Unidadgenero\\OneDrive - ine.gob.gt\\Documentos\\
 BasesSalud <- "C:\\Users\\Unidadgenero\\OneDrive - ine.gob.gt\\Documentos\\Github\\CompendioGenero2023\\Codigo\\Bases\\datos_administrativos\\Indicadores_de_Género\\SALUD\\"
 directorioGraficas <- "C:\\Users\\Unidadgenero\\OneDrive - ine.gob.gt\\Documentos\\Github\\CompendioGenero2023\\Codigo\\Graficas\\"
 
+anual(color1 = rgb(54,50,131, maxColorValue = 255), color2 = rgb(116, 112, 200, maxColorValue = 255))
 
 ############################################################################
 ###                                                                      ###
@@ -61,6 +62,13 @@ Tabla2_01 <- tablaLaTeX(c2_01, ruta = paste0(directorioGraficas, "Tabla2_01.tex"
 # 2.2.	Población entre 15 y 49 años que han usado algún método de             #
 # planificación familiar por sexo                                              #
 ################################################################################
+
+Datos_Salud <- paste0(BasesSalud, "CapSalud.xlsx")
+
+c2_02 <- data.frame(read.xlsx(xlsxFile = Datos_Salud, sheet = "2_2"))
+
+Tabla2_02 <- tablaLaTeX(c2_02,nombre_grupos = c(" ", "Usuarios nuevos" = 2, "Usuarios reconsultas" = 2), 
+                        opacidad_filas = 0.5, ruta = paste0(directorioGraficas, "Tabla2_02.tex"))
 
 ################################################################################
 # 2.3.	Población entre 15 y 49 años que han usado algún método de             #
